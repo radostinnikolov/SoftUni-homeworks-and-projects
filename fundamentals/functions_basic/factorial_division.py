6c3b2f529bddd8aa22d1bff11e2104b8b4caab73
bitcrusher55 <radostin.nikolov25@gmail.com>
2022-06-11 13:05:32 +0300

    Tribonacci seq

--- fundamentals/functions_basic/factorial_division.py
+++ fundamentals/functions_basic/factorial_division.py
@@ -3,6 +3,7 @@ def factorial(number):
         return 1
     return number * factorial(number - 1)
 
+
 first_num = int(input())
 second_num = int(input())
 print(f"{factorial(first_num) // factorial(second_num):.2f}")
